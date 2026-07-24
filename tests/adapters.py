428dc3d5580c8ec61cd8c1b7d4c15fb6d5d19b80
RashikShahjahan <rashikshahjahan@protonmail.com>
2026-07-24 17:17:12 -0500

    rmsnorm

--- tests/adapters.py
+++ tests/adapters.py
@@ -9,7 +9,7 @@ import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 from cs336_basics.tokenizer import train_bpe, Tokenizer
-from cs336_basics.model import linear, embedding
+from cs336_basics.model import linear, embedding, rmsnorm
 
 def run_linear(
     d_in: int,
@@ -386,7 +386,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    layer = rmsnorm.RMSNorm(d_model, eps)
+    layer.load_state_dict({"gain":weights})
+    return layer(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
